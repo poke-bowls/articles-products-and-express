64fb2bf3bfe71e9ce3df1ff9cd1e33439daf3f23
Micah Koki <micah.koki@gmail.com>
2016-01-15 19:55:21 -1000

    modularized products.js

--- db/products.js
+++ db/products.js
@@ -1,3 +1,35 @@
-module.exports = {
-  "products":[]
-};
+module.exports = (function(){
+  var products = [];
+
+  function _add( obj ) {
+    if( products.every( function( element ) {
+      return element.name !== obj.name;
+    })) {
+      products.push( obj );
+      return products;
+    }
+  }
+
+  function _getByName( name ) {
+    for( var k = 0; k < products.length; k++ ) {
+      if( products[k].name === name ) {
+        return products[k];
+      }
+    }
+  }
+
+  function _editByName( obj ) {
+    var changeIt = this.getByName( obj.name );
+    for( var key in obj ) {
+      changeIt[key] = obj[key];
+    }
+    return changeIt;
+  }
+
+  return {
+    products: products,
+    add: _add,
+    getByName: _getByName,
+    editByName: _editByName
+  };
+})();
--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var productMod = require( './db/products.js' );
+
+var obj = {
+  name: 'potato wedges',
+  price: 0.99
+};
+
+var myObj = {
+  name: 'potato wedges',
+  price: 0.49,
+  crispy: false,
+};
+
+console.log(productMod.add( obj ));
+console.log( productMod.getByName( 'potato wedges' ) );
+console.log( productMod.editByName( myObj ) );
 
 app.use(bodyParser.urlencoded({ extended : true }));
 
@@ -14,7 +30,6 @@ app.post('/products', function(req, res){
     res.send({ "success": false });
   }
 });
-
 
 app.set('views', './templates');
 app.set('view engine', 'jade');
